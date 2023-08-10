bc1514bba3a69b137faa75551a9679145c3dd49e
ElenLaptop <elen98725@gmail.com>
2023-08-10 20:06:45 +0800

    modify the trigger rule of straight

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ Global variable
 hand_ranking : dict
     A dictionary that returns the rank of a hand pattern.
 
+    
 Functions
 ----------
 evaluate_cards -- Evaluate all available hands that can be made.
@@ -111,7 +112,6 @@ def ind_higher_ranking(hand1: Hand, hand2: Hand) -> Literal[0, 1, 2]:
     if rank1 == rank2: result = 0
     return result
 
-CARDCOUNT = 31
 def evaluate_cards(cards: tuple[int] | list[int],
                    reprc_left: list[int] = []) -> list[Hand]:
     """Evaluate all available hands that can be made.
@@ -148,10 +148,10 @@ def evaluate_cards(cards: tuple[int] | list[int],
             value = sum(_reprc)
             suit = -1
             avaliable.append( Hand(cards, rank, value, suit) )
-        ####  straight  ####
-        if  (cards[2] - cards[1] == 1 and cards[1] - cards[0] == 1) or\
-            (cards[2] - cards[1] == 1 and (cards[0] + CARDCOUNT) - cards[2] == 1) or\
-            (cards[0] - (cards[2] - CARDCOUNT) == 1 and cards[1] - cards[0] == 1):
+        ####  straight  #### 
+        if (cards[2]-cards[1] == 1 and cards[1]-cards[0] == 1) or\
+           (cards[0] == 1 and cards[1] == 30 and cards[2] == 31) or\
+           (cards[0] == 1 and cards[1] == 2  and cards[2] == 31):
             rank = 'straight'
             value = sum([max(int(digit) for digit in str(card)) for card in cards])
             suit = -1
