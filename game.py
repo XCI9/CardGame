5f1b4b1683262802fbf56abfb192d98786beb57f
ElenLaptop <elen98725@gmail.com>
2023-08-14 23:41:27 +0800

    add PlayerUtilities.avalhands_info

--- game.py
+++ game.py
@@ -8,26 +8,25 @@ class PlayerUtility:
         self.for_erase = False
         # UI
         self.avalhands = []
-        self.avalhands_playable = []
-        self.prevhand_of_table = None
-        self.cards_hold = []
-        self.cards_ontable = []
+        self.avalhands_info = []
     def update_handsinfo(self):
         """Update information about hands, avaliable and playable.
 
         It evaluate avaliable hands, set value of `self.avalhands` to them.
         For n-th element of `self.avalhands` is playable for the table, n-th 
-        element of `self.avalhands_playable` will be True, False otherwise.
+        element of `self.avalhands_info` will be 'playable', it contains
+        the reason of unplayable otherwise.
         """
         self.avalhands = []
-        self.avalhands_playable = []
+        self.avalhands_info = []
         avalhands = evaluate_cards(self.select_cards)
         for avalhand in avalhands:
             self.side_bar.append(avalhand)
-            if self.table.is_playable_hand(avalhand):
-                self.avalhands_playable.append(True)
+            playable, info = self.table.is_playable_hand(avalhand)
+            if playable:
+                self.avalhands_info.append('playable')
             else:
-                self.avalhands_playable.append(False)
+                self.avalhands_info.append(info)
     def select_cards(self, cards: tuple) -> bool:
         """Call this function when a player change selected cards."""
         if any(card not in self.player.cards for card in cards):
@@ -39,9 +38,9 @@ class PlayerUtility:
     def select_hand(self, index: int) -> bool:
         """Call this function when a player select a hand.
         Argument: index, int
-            index to acess `self.avalhands` and `self.avalhands_playable`.
+            index to acess `self.avalhands` and `self.avalhands_info`.
         """
-        if not self.avalhands_playable[index]:
+        if not self.avalhands_info[index] == 'playable':
             return False
         self.player.selected_hand = self.avalhands[index]
         return True
@@ -80,5 +79,6 @@ class GameCore:
     
     def start(self):
         self.table.start()
+
         for player in self.table.players:
             self.p.append(PlayerUtility(player, self.table))
